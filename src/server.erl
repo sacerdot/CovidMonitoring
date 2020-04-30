3b99b4f322abb93c629c6fc66fedb66d9e66dd7c
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-04-30 19:19:59 +0200

    server

--- src/server.erl
+++ src/server.erl
@@ -1,17 +1,34 @@
 -module(server).
--export([main/0, test/0]).
+-export([main/0, init/1, luogo/0, sleep/1]).
 
-test() ->
+sleep(T) ->
+  receive after T -> ok end.
+
+init(L) ->
   receive
-    {test_me, PID} ->
-      io:format("~p require a test~n", [PID]),
-      case rand:uniform(4) of
-        1 -> PID ! positive;
-        _ -> PID ! negative
-      end, test()
+    {new_place, PID} ->
+      io:format("msg from ~p ~n", [PID]),
+      monitor(process, PID),
+      init([ PID | L ]);
+    Msg = {DOWN, Reference, process, Pid, Reason} ->
+      io:format("~p monitor1 ~n", [Msg]),
+      init(L);
+    M = _ ->
+      io:format("~p monitor2 ~n", [M])
   end.
 
- 
+luogo() ->
+  global:send(server, {new_place, self()}) ,
+  sleep(5000),
+  exit (ciccio).
+
 main() ->
-  global:register_name(hospital, self()),
-  spawn(?MODULE, test, []).
+  L = [],
+  H = spawn(?MODULE, init, [L]),
+  global:register_name(server, H),
+  L1 = spawn(?MODULE, luogo, []),
+%%  io:format("~p Luogo 1 ~n", [L1]),
+  L2 = spawn(?MODULE, luogo, []),
+%%  io:format("~p Luogo 2 ~n", [L2]),
+  exit(L2,kill).
+
