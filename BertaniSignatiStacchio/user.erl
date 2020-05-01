fc6503ad9069aa29c7537c4a48f27e8c71ed9985
Lorenzo Stacchio <36449327+lorenzo-stacchio@users.noreply.github.com>
2020-05-01 15:56:22 +0200

    Add location_manteiner functions
    - Location mantainer;
    - Get_places;
    - generate_N_random_from_list.

--- BertaniSignatiStacchio/user.erl
+++ BertaniSignatiStacchio/user.erl
@@ -8,6 +8,58 @@
 %%%-------------------------------------------------------------------
 -module(user).
 -author("Lorenzo_Stacchio").
-
 %% API
--export([]).
+-export([test/0, location_manteiner/1, server/1]).
+
+sleep(N) -> receive after N -> ok end.
+
+
+server(L) ->
+  io:format("Lista totale server ~p~n", [L]),
+  receive
+    {get_places, P} -> io:format("Messaggio ricevuto in server ~n", [])
+      , P ! {places, L}
+      , server(L)
+  end.
+
+
+generate_N_random_from_list(LR, N, LIST_TO_RETURN) ->
+  C = rand:uniform(N),
+  io:format("Random ~p,~p,~p,~p~n", [LR, N, LIST_TO_RETURN, C]),
+  if
+    length(LIST_TO_RETURN) < N ->
+      generate_N_random_from_list(lists:delete(lists:nth(C,LR), LR), N , lists:append(LIST_TO_RETURN, [lists:nth(C,LR)]));
+    true -> LIST_TO_RETURN
+  end.
+
+
+get_places(L, P) ->
+  if
+    length(L) < 3 -> io:format("In if loop~p~p~n", [L, length(L)]),
+      server ! {get_places, self()},
+      receive
+        {places, LR} -> io:format("Updatedlist~p~p~n", [LR, length(LR)]),
+          P ! {places, generate_N_random_from_list(LR, 3, [])}
+      end;
+    true -> ok
+  end.
+
+
+location_manteiner(L) ->
+  io:format("Lista totale normale~p~p~n", [L, length(L)]),
+  sleep(3000),
+  get_places(L, self()),
+  receive
+    {places, LR} -> location_manteiner(LR)
+  end.
+
+
+test() ->
+  %mettere link al server
+  S = spawn_link(?MODULE, server, [lists:seq(0, 5)]),
+  register(server, S), %rendo pubblico associazione nome PID
+  %server ! {get_places, self()},
+  M = spawn_link(?MODULE, location_manteiner, [[]]),
+  io:fwrite("Manteiner spawnata\n").
+
+
