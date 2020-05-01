e066e0ccdb187e1e187cbfa1f6ca4654eb2f74fb
Federico Bertani <fedbertani@gmail.com>
2020-05-01 16:33:09 +0200

    debug prints like there is no tomorrow

--- central_server.erl
+++ central_server.erl
@@ -13,17 +13,26 @@
 -export([run/0]).
 
 run() ->
+  io:format("central server started ~n"),
   % register central server name
   global:register_name/2(server,self()),
+  io:format("registered server global name ~n"),
   % trapping exit signal from users
   process_flag(trap_exit, true),
+  io:format("set flag to trap exit ~n"),
   PIDLIST = [],
+  io:format("created list for PIDs ~n"),
   receive
     % a new place is registering
     {new_place,Place_pid} ->
+      io:format("Place ~p want to register ~n",[Place_pid]),
       PIDLIST ++ [Place_pid];
     % user requested list of places
-    {get_places, Pid} -> Pid ! {places, PIDLIST};
+    {get_places, Pid} ->
+      io:format("User ~p requested places list ~n",[Pid]),
+      Pid ! {places, PIDLIST};
     % remove from places list dead place
-    {'EXIT',FromPid,Reason} -> PIDLIST = lists:delete(FromPid,PIDLIST)
+    {'EXIT',FromPid,Reason} ->
+      io:format("Place ~p is dead ~n",[FromPid]),
+      PIDLIST = lists:delete(FromPid,PIDLIST)
    end.
--- hospital.erl
+++ hospital.erl
@@ -13,13 +13,20 @@
 -export([run/0]).
 
 run() ->
+  io:format("hospital started~n"),
   global:register_name(hospital,self()),
+  io:format("hospital registered~n"),
   receive
     % an user want to be tested
     {test_me, PID} ->
+      io:format("process ~p want to be tested ~n",[PID]),
       % answer with probability 25% to be positive
-      case (rand:uniform()=<0.25) of
-        true -> PID ! positive;
-        false -> PID ! negative
+      case (rand:uniform(4)==1) of
+        true ->
+          io:format("positive! ~n"),
+          PID ! positive;
+        false ->
+          io:format("negative! ~n"),
+          PID ! negative
       end
   end.
