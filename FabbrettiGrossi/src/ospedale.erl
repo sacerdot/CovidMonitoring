e042225ebaf586837ffc2281dfb2e9f819ce52f3
gfabbretti8 <g.fabbretti8@gmail.com>
2020-05-19 13:30:53 +0200

    indentazione codice

--- FabbrettiGrossi/src/ospedale.erl
+++ FabbrettiGrossi/src/ospedale.erl
@@ -5,22 +5,22 @@
 start() ->
     Result = global:register_name(hospital, self()),
     case Result of
-	yes -> 
-	    io:format("L'ospedale si è registrato correttamente.~n"),
-	    loop();
-	no -> 
-	    io:format("Qualcosa è andato storto nella registrazione dell'ospedale.~n")
+        yes -> 
+            io:format("L'ospedale si è registrato correttamente.~n"),
+            loop();
+        no -> 
+            io:format("Qualcosa è andato storto nella registrazione dell'ospedale.~n")
     end.
 
 loop() ->
     receive
-	{test_me, Pid} ->
-	    Result = rand:uniform(4),
-	    case Result of
-		4 -> Pid ! positive;
-		_ -> Pid ! negative
-	    end,
-	    loop();
-	Other -> io:format("Unexpected message: ~p~n", [Other]),
-		 loop()
+        {test_me, Pid} ->
+            Result = rand:uniform(4),
+            case Result of
+                4 -> Pid ! positive;
+                _ -> Pid ! negative
+            end,
+            loop();
+        Other -> io:format("Unexpected message: ~p~n", [Other]),
+                 loop()
     end.
