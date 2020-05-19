6b6f4502e9459ac3482ec2f0d06e663ffa9fc53c
gfabbretti8 <g.fabbretti8@gmail.com>
2020-05-19 18:21:58 +0200

    aggiunte stampe per il debug

--- FabbrettiGrossi/src/luoghi.erl
+++ FabbrettiGrossi/src/luoghi.erl
@@ -1,67 +1,86 @@
 -module(luoghi).
--export([start/0]).
+-export([start/0, place_manager/1, contact_manager/1]).
 
 
-place_manager() ->
-  receive
-    new_visitor ->
-      Result = rand:uniform(10),
-      case Result of
-        10 -> 
-	      %DEBUG
-	      io:format("Il posto chiude normalmente dopo una visita di un utente.~n"),
-	      exit(normal);   % close place
-        _ -> place_manager()  % keep open
-      end
+place_manager(Manager) ->
+    receive
+        new_visitor ->
+            Result = rand:uniform(10),
+            case Result of
+                10 ->
+                    %DEBUG
+                    Manager ! {debug, place_close_normally},
+                    exit(normal);   % close place
+                _ ->
+                    place_manager(Manager)  % keep open
+            end
+    end.
+
+contact(_, _, []) -> ok;
+contact(Manager, NEW_VISITOR, [VISITOR | OTHER_VISITORS]) ->
+    Result = rand:uniform(4),
+    case Result of
+        4 ->
+            {_, PID_VISITOR} = VISITOR,
+            NEW_VISITOR ! {contact, PID_VISITOR},
+            PID_VISITOR ! {contact, NEW_VISITOR},
+            contact(Manager, NEW_VISITOR, OTHER_VISITORS);
+
+        _ -> contact(Manager, NEW_VISITOR, OTHER_VISITORS)
   end.
 
-contact(_, []) -> ok;
-contact(NEW_VISITOR, [VISITOR | OTHER_VISITORS]) ->
-  Result = rand:uniform(4),
-  case Result of
-    4 ->
-      {_, PID_VISITOR} = VISITOR,
-      NEW_VISITOR ! {contact, PID_VISITOR},
-      PID_VISITOR ! {contact, NEW_VISITOR},
-      contact(NEW_VISITOR, OTHER_VISITORS);
 
-    _ -> contact(NEW_VISITOR, OTHER_VISITORS)
-  end.
+contact_manager(Manager) ->
+    receive
+        {find_contacts, NEW_VISITOR, VISITORS_LIST} ->
+            contact(Manager, NEW_VISITOR, VISITORS_LIST)
+    end,
+  contact_manager(Manager).
 
-
-contact_manager() ->
-  receive
-    {find_contacts, NEW_VISITOR, VISITORS_LIST} ->
-      contact(NEW_VISITOR, VISITORS_LIST)
-  end,
-  contact_manager().
+debug(Message) ->
+    case Message of
+        {contact, Pid1, Pid2} ->
+            io:format("~p: Contatto tra ~p e ~p~n", [self(), Pid1, Pid2]);
+        place_close_normally ->
+            io:format("~p: chiude normalmente dopo una visita di un utente.~n", [self()]);
+        {begin_visit, Pid, Visitors} ->
+            VisitorList = [ L || {_, L} <- Visitors],
+            io:format("~p: Lista di visitatori nel luogo: ~p~n", [self(),VisitorList ++ [Pid]]),
+            io:format("~p: visitatore ~p ha iniziato la visita~n", [self(), Pid]);
+        {end_visit, Pid, Visitors} ->
+            VisitorList = [ L || {_, L} <- Visitors],
+            io:format("~p: visitatore ~p ha terminato la visita ~n", [self(), Pid]),
+            io:format("~p: Lista di visitatori nel luogo: ~p~n", [self(),VisitorList -- [Pid]])
+        end.
 
 
 update_visitors(VISITORS_LIST, PID_MANAGER) ->
-  receive
-      {'DOWN',_, process, _, normal} -> 
-	  exit(normal);
-      % VISIT PROTOCOL/1: begin visit
-      {begin_visit, PID_VISITATORE, REF} ->
-      %DEBUG
-      io:format("Lista di visitatori nel luogo: ~p~n", [VISITORS_LIST]),
-      io:format("Visitatore: ~p ha iniziato la visita di ~p~n", [PID_VISITATORE, self()]),
-      % get pid managers
-      {pid_manager, CM_pid, PM_pid} = PID_MANAGER,
-      % CONTACT PROTOCOL
-      CM_pid ! {find_contacts, PID_VISITATORE, VISITORS_LIST},
-      % LIFE CYCLE
-      PM_pid ! new_visitor,
+    receive
+        {debug, Message} ->
+            debug(Message),
+            update_visitors(VISITORS_LIST, PID_MANAGER);
+        {'DOWN',_, process, _, normal} ->
+            exit(normal);
+            % VISIT PROTOCOL/1: begin visit
+        {begin_visit, PID_VISITATORE, REF} ->
+            %DEBUG
+            self() ! {debug, {begin_visit, PID_VISITATORE, VISITORS_LIST}},
+            % get pid managers
+            {pid_manager, CM_pid, PM_pid} = PID_MANAGER,
+            % CONTACT PROTOCOL
+            CM_pid ! {find_contacts, PID_VISITATORE, VISITORS_LIST},
+                                                % LIFE CYCLE
+            PM_pid ! new_visitor,
 
-      % notify begin visit
-      update_visitors([{REF, PID_VISITATORE} | VISITORS_LIST], PID_MANAGER);
+            % notify begin visit
+            update_visitors([{REF, PID_VISITATORE} | VISITORS_LIST], PID_MANAGER);
 
-    % VISIT PROTOCOL/2: end visit
-    {end_visit, PID_VISITATORE, REF} ->
-	  %DEBUG
-         io:format("Visitatore: ~p ha terminato la visita di ~p~n", [PID_VISITATORE, self()]),
-      % notify end visit
-      update_visitors(VISITORS_LIST -- [{REF, PID_VISITATORE}], PID_MANAGER)
+            % VISIT PROTOCOL/2: end visit
+        {end_visit, PID_VISITATORE, REF} ->
+        %DEBUG
+            self() ! {debug, {end_visit, PID_VISITATORE, VISITORS_LIST}},
+            % notify end visit
+            update_visitors(VISITORS_LIST -- [{REF, PID_VISITATORE}], PID_MANAGER)
   end.
 
 
@@ -73,9 +92,9 @@ luogo() ->
   % INIT PROTOCOL/2: notify server
   Server ! {new_place, self()},
   % contact manager
-  {CM_pid, _} = spawn_monitor(fun contact_manager/0),
+  {CM_pid, _} = spawn_monitor(?MODULE, contact_manager, [self()]),
   % place manager
-  {PM_pid, _} = spawn_monitor(fun place_manager/0),
+  {PM_pid, _} = spawn_monitor(?MODULE, place_manager, [self()]),
   % update visitors
   update_visitors([],{pid_manager, CM_pid, PM_pid}).
 
