e6dde4d7f667e6d752c48c06b14f4a594b9da9aa
gfabbretti8 <g.fabbretti8@gmail.com>
2020-05-27 14:03:32 +0200

    aggiunta documentazione

--- FabbrettiGrossi/src/ospedale.erl
+++ FabbrettiGrossi/src/ospedale.erl
@@ -1,17 +1,25 @@
--module(ospedale).
--export([start/0, loop/0]).
+%%%-------------------------------------------------------------------
+%%% @doc Modulo ospedale per il progetto del corso di Paradigmi emergenti.
+%%% @end
+%%%-------------------------------------------------------------------
 
+-module(ospedale).
+-export([start/0]).
 
 start() ->
     Result = global:register_name(hospital, self()),
     case Result of
-        yes -> 
+        yes ->
             io:format("L'ospedale si è registrato correttamente.~n"),
             loop();
-        no -> 
+        no ->
             io:format("Qualcosa è andato storto nella registrazione dell'ospedale.~n")
     end.
-
+%%--------------------------------------------------------------------
+%% @doc L'ospedale testa gli utenti e con probabilita' 1/4 risponde
+%% risponde positive
+%% @end
+%%--------------------------------------------------------------------
 loop() ->
     receive
         {test_me, Pid} ->
