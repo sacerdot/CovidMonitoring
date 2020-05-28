f15d12b210b847c550301f085755a222e8255ec0
Luigi Grossi <luigig93@gmail.com>
2020-05-28 12:33:10 +0200

    Refactoring nomi variabili in camelCase e aggiunta documentazione e descrizione protocolli

--- FabbrettiGrossi/src/server.erl
+++ FabbrettiGrossi/src/server.erl
@@ -1,41 +1,57 @@
+%%%---------------------------------------------------------------------
+%%% @doc Modulo server per il progetto del corso di Paradigmi emergenti.
+%%% @end
+%%%---------------------------------------------------------------------
+
 -module(server).
 -export([start/0]).
 
-
-update_places(PIDLIST) ->
-    receive
-        % INIT PROTOCOL/1: keeps and monitors a place list
-        {new_place, PID_LUOGO} ->
-            update_places([PID_LUOGO | PIDLIST]);
-
-        {'EXIT', Pid, normal} ->
-            io:format("Sto per rimuovere ~p dalla lista dei luoghi~n", [Pid]),
-            update_places(PIDLIST -- [Pid]);
-
-        {'EXIT', Pid, positive} ->
-            io:format("~p: esce perche' positivo al test~n", [Pid]),
-            update_places(PIDLIST);
-
-        {'EXIT', Pid, quarantena} ->
-            io:format("~p: esce perche' va in quarantena~n", [Pid]),
-            update_places(PIDLIST);
-
-        {'EXIT', Pid, Reason} ->
-            io:format("~p exit because ~p~n", [Pid, Reason]),
-            update_places(PIDLIST -- [Pid]);
-
-        {get_places, PID} ->
-            PID ! {places, PIDLIST},
-            update_places(PIDLIST);
-
-        Other ->
-            io:format("Messaggio inaspettato: ~p~n", [Other]),
-            update_places(PIDLIST)
-  end.
-
+%%%-------------------------------------------------------------------
+%%% @doc PROTOCOLLO DI INIZIALIZZAZIONE:
+%%% all'avvio si registra globalmente.
+%%% @end
+%%%-------------------------------------------------------------------
 
 start() ->
     global:register_name(server,self()),
     io:format("Io sono il server~n",[]),
     process_flag(trap_exit, true),
     update_places([]).
+
+%%%----------------------------------------------------------------------------
+%%% @doc PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA:
+%%% 1) Mantiene una lista dei luoghi attivi.
+%%% 2) Monitora i luoghi attivi, eliminandoli dalla lista quando questi muoiono
+%%% 3) Risponde alle richieste (degli utenti) di conoscenza sui luoghi.
+%%% @end
+%%%----------------------------------------------------------------------------
+
+update_places(PidList) ->
+    receive
+        {new_place, PidPlace} ->
+            update_places([PidPlace | PidList]);
+
+        {'EXIT', Pid, normal} ->
+            io:format("Sto per rimuovere ~p dalla lista dei luoghi~n", [Pid]),
+            update_places(PidList -- [Pid]);
+
+        {'EXIT', Pid, positive} ->
+            io:format("~p: esce perche' positivo al test~n", [Pid]),
+            update_places(PidList);
+
+        {'EXIT', Pid, quarantena} ->
+            io:format("~p: esce perche' va in quarantena~n", [Pid]),
+            update_places(PidList);
+
+        {'EXIT', Pid, Reason} ->
+            io:format("~p exit because ~p~n", [Pid, Reason]),
+            update_places(PidList -- [Pid]);
+
+        {get_places, Pid} ->
+            Pid ! {places, PidList},
+            update_places(PidList);
+
+        Other ->
+            io:format("Messaggio inaspettato: ~p~n", [Other]),
+            update_places(PidList)
+  end.
