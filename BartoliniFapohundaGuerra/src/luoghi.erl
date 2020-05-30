23f7a1bb06de146de295216ca2590a38e1bd394b
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-05-31 01:28:06 +0200

    Mods as suggested during exam

--- BartoliniFapohundaGuerra/src/luoghi.erl
+++ BartoliniFapohundaGuerra/src/luoghi.erl
@@ -2,7 +2,7 @@
 -export([start/0, luogo/0, init_luogo/1, visit_place/2]).
 -import(utils, [sleep/1, set_subtract/2, make_probability/1, check_service/1]).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI INIZIALIZZAZIONE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI INIZIALIZZAZIONE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 init_luogo(Prob) ->
   PidServer = check_service(server),
   PidServer ! {ciao, da, luogo, self()},
@@ -11,53 +11,58 @@ init_luogo(Prob) ->
   PidServer ! {new_place, self()},
   visit_place([], Prob).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI VISITA DEI LUOGHI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+visit_place(L, Probs) ->
+  receive
+    {begin_visit, PID, Ref} ->
+      MRef = monitor(process, PID),
+      % PidOldUser can die
+      [contact(PID, PidOldUser, Probs(contact_tracing)) || {PidOldUser, _, _} <- L],
+      check_for_closing(Probs(check_for_closing)),
+      visit_place([{PID, Ref, MRef} | L], Probs);
+    {end_visit, PID, Ref} ->
+      case [MR || {Pid, _, MR} <- L, Pid =:= PID] of
+        [MRef] ->
+          demonitor(MRef),
+          visit_place(set_subtract(L, [{PID, Ref, MRef}]), Probs);
+        _ -> visit_place(L, Probs)
+      end;
+    {'DOWN', MRef, process, PidExit, Reason} ->
+      case [{R, MR} || {Pid, R, MR} <- L, Pid =:= PidExit] of
+        [{Ref, MRef}] ->
+          io:format("[Luogo] ~p Utente ~p con Ref  ~p morto per ~p ~n", [self(), PidExit, Ref, Reason]),
+          visit_place(set_subtract(L, [{PidExit, Ref, MRef}]), Probs);
+        _ ->
+          io:format("[Luogo] ~p Utente ~p non presente in lista~n", [self(), PidExit]),
+          visit_place(L, Probs)
+      end
+  end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI RILEVAMENTO DEI CONTATTI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+contact(NewUser, PidOldUser, Prob) ->
+  case Prob() of
+    true ->
+      NewUser ! {contact, PidOldUser},
+      io:format("[Luogo] ~p Contatto da ~p a ~p~n", [self(), NewUser, PidOldUser]);
+    false -> ok
+  end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% CICLO DI VITA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+check_for_closing(Prob) ->
+  case Prob() of
+    true -> exit(normal);
+    false -> ok
+  end.
+
 get_probs() ->
   Probs = #{contact_tracing => make_probability(25), check_for_closing => make_probability(10)},
   fun(X) ->
     maps:get(X, Probs)
   end.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI VISITA DEI LUOGHI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-visit_place(L, Probs) ->
-  receive
-    {begin_visit, PID, Ref} ->
-      MRef = monitor(process, PID),
-      % PidOldUser can die
-      contact_tracing(PID, [PidOldUser || {PidOldUser, _, _} <- L], Probs(contact_tracing)),
-      check_for_closing(Probs(check_for_closing)),
-      visit_place([{PID, Ref, MRef} | L], Probs);
-    {end_visit, PID, Ref} ->
-      [MRef] = [MR || {Pid, _, MR} <- L, Pid =:= PID],
-      demonitor(MRef),
-      visit_place(set_subtract(L, [{PID, Ref, MRef}]), Probs);
-    {'DOWN', MRef, process, PidExit, Reason} ->
-      [{Ref, MRef}] = [{R, MR} || {Pid, R, MR} <- L, Pid =:= PidExit],
-      io:format("[Luogo] ~p Utente ~p con Ref  ~p morto per ~p ~n", [self(), PidExit, Ref, Reason]),
-      NL = set_subtract(L, [{PidExit, Ref, MRef}]),
-      visit_place(NL, Probs)
-  end.
-
-%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI RILEVAMENTO DEI CONTATTI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-contact_tracing(_, [], _) -> ok;
-contact_tracing(NewUser, [PidOldUser | T], Prob) ->
-  case Prob() of
-    1 ->
-      NewUser ! {contact, PidOldUser},
-      io:format("[Luogo] ~p Contatto da ~p a ~p~n", [self(), NewUser, PidOldUser]);
-    _ -> ok
-  end,
-  contact_tracing(NewUser, T, Prob).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CICLO DI VITA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-check_for_closing(Prob) ->
-  case Prob() of
-    1 -> exit(normal);
-    _ -> ok
-  end.
-
-start() ->
-  [spawn(fun luogo/0) || _ <- lists:seq(1, 1000)].
-
 luogo() ->
   io:format("Io sono il luogo ~p~n", [self()]),
   init_luogo(get_probs()).
+
+start() ->
+  [spawn(fun luogo/0) || _ <- lists:seq(1, 1000)].
--- BartoliniFapohundaGuerra/src/utenti.erl
+++ BartoliniFapohundaGuerra/src/utenti.erl
@@ -1,8 +1,8 @@
 -module(utenti).
 -export([main/0, list_handler/2, check_list/2, visit_place/2, actorDispatcher/0, require_test/2, utente/0, start/0]).
--import(utils, [sleep/1, set_subtract/2, take_random/2, check_service/1, make_probability/1]).
+-import(utils, [sleep/1, set_subtract/2, get_random/2, check_service/1, make_probability/1]).
 
-%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (a) %%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (a) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 main() ->
   PidServer = check_service(server),
   PidServer ! {ciao, da, utente, self()},
@@ -13,48 +13,59 @@ actorDispatcher() ->
   process_flag(trap_exit, true),
   ActorListHandler = spawn_link(?MODULE, list_handler, [self(), []]),
   %io:format("[ActorDispatcher] User ~p ActorListHandler ~p ~n", [self(), ActorListHandler]),
-  spawn_link(?MODULE, check_list, [ActorListHandler, self()]),
+  ActorCheckList = spawn_link(?MODULE, check_list, [ActorListHandler, self()]),
   %io:format("[ActorDispatcher] User ~p CheckList ~p ~n", [self(), CheckList]),
   ActorVisitPlace = spawn_link(?MODULE, visit_place, [ActorListHandler, self()]),
   %io:format("[ActorDispatcher] User ~p ActorVisitPlace ~p ~n", [self(), ActorVisitPlace]),
   ActorRequireTest = spawn_link(?MODULE, require_test, [self(), make_probability(25)]),
   %io:format("[ActorDispatcher] User ~p ActorRequireTest ~p ~n", [self(), ActorRequireTest]),
   %sleep(1000),
-  dispatcher_loop(ActorListHandler, ActorRequireTest, ActorVisitPlace).
+  dispatcher_loop(ActorListHandler, ActorRequireTest, ActorVisitPlace, ActorCheckList).
 
-dispatcher_loop(ListHandler, RequiredTest, VisitPlace) ->
+dispatcher_loop(ListHandler, RequireTest, VisitPlace, CheckList) ->
   receive
     {places, PIDLIST} ->
-      ListHandler ! {get_list, self()},
-      receive
-        {list, L} ->
-          R = set_subtract(PIDLIST, L),
-          case length(L) of
-            0 -> ListHandler ! {update_list, take_random(R, 3)};
-            1 -> ListHandler ! {update_list, take_random(R, 2)};
-            2 -> ListHandler ! {update_list, take_random(R, 1)};
-            _ -> ok
-          end
-      end;
+      update_list(ListHandler, PIDLIST),
+      dispatcher_loop(ListHandler, RequireTest, VisitPlace, CheckList);
     {contact, PID} ->
-      % check if PID is alive before linking
-      case erlang:process_info(PID) == undefined of
-        true -> io:format("[Dispatcher] ~p impossibile linkarsi a ~p~n", [self(), PID]);
-        false ->
-          link(PID),
-          io:format("[Dispatcher] ~p linkato a ~p~n", [self(), PID])
-      end;
+      link_user(PID),
+      dispatcher_loop(ListHandler, RequireTest, VisitPlace, CheckList);
     positive ->
       io:format("[Dispatcher] ~p sono positivo ~n", [self()]),
       exit(positive);
     negative ->
-      io:format("[Dispatcher] ~p Sono negativo ~n", [self()]);
+      io:format("[Dispatcher] ~p Sono negativo ~n", [self()]),
+      dispatcher_loop(ListHandler, RequireTest, VisitPlace, CheckList);
+    {'EXIT', _, X} when X =:= quarantine; X =:= positive ->
+      io:format("[Dispatcher] ~p entro in quarantena ~n", [self()]),
+      exit(quarantine);
+    {'EXIT', RequireTest, R} ->
+      io:format("[Dispatcher] ~p RequireTest morto: ~p da ~p ~n", [self(), R, RequireTest]),
+      dispatcher_loop(ListHandler, spawn_link(?MODULE, require_test, [self(), make_probability(25)]), VisitPlace, CheckList);
+    {'EXIT', VisitPlace, R} ->
+      io:format("[Dispatcher] ~p VisitPlace morto: ~p da ~p ~n", [self(), R, VisitPlace]),
+      dispatcher_loop(ListHandler, RequireTest, spawn_link(?MODULE, visit_place, [ListHandler, self()]), CheckList);
+    {'EXIT', CheckList, R} ->
+      io:format("[Dispatcher] ~p CheckList morto: ~p da ~p ~n", [self(), R, CheckList]),
+      dispatcher_loop(ListHandler, RequireTest, VisitPlace, spawn_link(?MODULE, check_list, [ListHandler, self()]));
+    {'EXIT', ListHandler, R} ->
+      io:format("[Dispatcher] ~p ListHandler morto: ~p da ~p ~n", [self(), R, ListHandler]),
+      dispatcher_loop(spawn_link(?MODULE, list_handler, [self(), []]), RequireTest, VisitPlace, CheckList);
     {'EXIT', Sender, R} ->
-      handle_exit_messages(R, self(), Sender)
-  end,
-  dispatcher_loop(ListHandler, RequiredTest, VisitPlace).
+      io:format("[Dispatcher] ~p catturato evento: ~p da ~p ~n", [self(), R, Sender]),
+      exit(R)
+  end.
 
-%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (b) %%%%%%%%%%%%%%%%%%%%
+link_user(PID) ->
+  % check if PID is alive before linking
+  case erlang:process_info(PID) == undefined of
+    true -> io:format("[Dispatcher] ~p impossibile linkarsi a ~p~n", [self(), PID]);
+    false ->
+      link(PID),
+      io:format("[Dispatcher] ~p linkato a ~p~n", [self(), PID])
+  end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (b) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 list_handler(PidDispatcher, L) ->
   receive
     {get_list, Pid} ->
@@ -70,59 +81,50 @@ list_handler(PidDispatcher, L) ->
       list_handler(PidDispatcher, set_subtract(L, [Pid]))
   end.
 
-handle_exit_messages(R, PidDispatcher, Pid) ->
-  case R of
-    quarantine ->
-      io:format("[Dispatcher] ~p entro in quarantena ~n", [PidDispatcher]),
-      exit(quarantine);
-    positive ->
-      io:format("[Dispatcher] ~p entro in quarantena ~n", [PidDispatcher]),
-      exit(quarantine);
-    _ ->
-      io:format("[Dispatcher] ~p catturato evento: ~p da ~p ~n", [PidDispatcher, R, Pid]),
-      exit(R)
+update_list(ListHandler, PIDLIST) ->
+  L = get_list(ListHandler),
+  R = set_subtract(PIDLIST, L),
+  ListHandler ! {update_list, get_random(R, 3 - length(L))}.
+
+get_list(ListHandler) ->
+  ListHandler ! {get_list, self()},
+  receive
+    {list, L} -> L
   end.
 
-%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (c, d) %%%%%%%%%%%%%%%%%%%%
-check_list(ActorList, PidDispatcher) ->
-  ActorList ! {get_list, self()},
-  receive
-    {list, L} ->
-      case length(L) >= 3 of
-        true -> ok;
-        false ->
-          global:send(server, {get_places, PidDispatcher})
-      end
+%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI MANTENIMENTO DELLA TOPOLOGIA (c, d) %%%%%%%%%%%%%%%%%%%%%%%%%%
+check_list(ListHandler, PidDispatcher) ->
+  L = get_list(ListHandler),
+  case length(L) < 3 of
+    true -> global:send(server, {get_places, PidDispatcher});
+    false -> ok
   end,
   sleep(10000),
-  check_list(ActorList, PidDispatcher).
+  check_list(ListHandler, PidDispatcher).
 
-%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI VISITA DEI LUOGHI %%%%%%%%%%%%%%%%%%%%
-visit_place(ActorList, PidDispatcher) ->
-  ActorList ! {get_list, self()},
-  receive
-    {list, L} ->
-      case length(L) >= 1 of
-        true ->
-          REF = make_ref(),
-          [LUOGO | _] = take_random(L, 1),
-          LUOGO ! {begin_visit, PidDispatcher, REF},
-          sleep(5000 + rand:uniform(5000)),
-          LUOGO ! {end_visit, PidDispatcher, REF};
-        false -> ok
-      end,
-      sleep(3000 + rand:uniform(2000)),
-      visit_place(ActorList, PidDispatcher)
-  end.
+%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI VISITA DEI LUOGHI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+visit_place(ListHandler, PidDispatcher) ->
+  L = get_list(ListHandler),
+  case length(L) >= 1 of
+    true ->
+      REF = make_ref(),
+      [LUOGO | _] = get_random(L, 1),
+      LUOGO ! {begin_visit, PidDispatcher, REF},
+      sleep(5000 + rand:uniform(5000)),
+      LUOGO ! {end_visit, PidDispatcher, REF};
+    false -> ok
+  end,
+  sleep(3000 + rand:uniform(2000)),
+  visit_place(ListHandler, PidDispatcher).
 
-%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI TEST (a,b) %%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%% PROTOCOLLO DI TEST (a,b) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 require_test(PidDispatcher, Probability) ->
   sleep(30000),
   case Probability() of
-    1 ->
+    true ->
       PidOspedale = check_service(ospedale),
       PidOspedale ! {test_me, PidDispatcher};
-    _ -> ok
+    false -> ok
   end,
   require_test(PidDispatcher, Probability).
 
